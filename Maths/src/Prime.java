b1de78798758dfca949f1eded9bd700a26fc4f02
venkatanagapavankumarcherukuri <pk91684@gmail.com>
2023-04-05 02:33:41 -0500

    recursion

---
+++ Maths/src/Prime.java
@@ -0,0 +1,17 @@
+public class Prime {
+
+    public static void main(String[] args) {
+        int n=41;
+        System.out.println(isPrime(n));
+    }
+
+    private static boolean isPrime(int n) {
+        if(n<=1) return false;
+        int c=2;
+        while(c*c<=n){
+            if(n%c==0)return false;
+            c++;
+        }
+        return true;
+    }
+}
